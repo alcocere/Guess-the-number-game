60050e31885c14aa08d834eed87b2ff78e769b7e
alcocere <elisa.alcocerm@gmail.com>
2021-01-28 11:09:28 +0100

    add updateclicks js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Generar un número al azar entre 1 y 100 y mostrat en la consola
 const random = getRandomNumber (100);
 const btnElement = document.querySelector('.js-button');
 const inputElement = document.querySelector('.js-input');
@@ -13,16 +12,19 @@ function getRandomNumber(max) {
 }
 console.log("Mi número aleatorio es " + random);
 
+function updateAttempts (){
+    acc += 1;
+    attempts.innerHTML = `Número de intentos: ${acc}`;
+}
 
 function updateClick () {
     let inputValue = parseInt(inputElement.value);
-    inputElement.innerHTML = inputValue;
 
     if (inputValue < 1 || inputValue > 100) {
         cluesElement.innerHTML = "El número deber ser entre el 1 y 100, vuelve a intentar";
     } else if (inputValue === random) {
         cluesElement.innerHTML ="¡Has ganado campeona!";
-    } else if (inputElement > random) {
+    } else if (inputValue > random) {
         cluesElement.innerHTML = "Pista : Demasiado alto... prueba otra vez!"
     } else {
         cluesElement.innerHTML = "Pista: Demasiado bajo...prueba con otro número!"
