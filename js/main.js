970440ee1c2d48fd7638435ab0330399426d5edc
alcocere <elisa.alcocerm@gmail.com>
2021-02-15 19:48:46 +0100

    Add counter & correct issues

--- js/main.js
+++ js/main.js
@@ -1,36 +1,58 @@
 "use strict";
 
-const random = getRandomNumber (100);
 const btnElement = document.querySelector('.js-button');
 const inputElement = document.querySelector('.js-input');
 const cluesElement = document.querySelector('.js-clues');
-const attempts = document.querySelector (".js-attempts");
-
+const attemptsElement = document.querySelector(".js-attempts");
+const formElement = document.querySelector('.js-form');
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
+const random = getRandomNumber(100);
 console.log("Mi número aleatorio es " + random);
 
-function updateAttempts (){
-    acc += 1;
-    attempts.innerHTML = `Número de intentos: ${acc}`;
+function handleForm(ev) {
+    ev.preventDefault();
 }
 
-function updateClick () {
+formElement.addEventListener('submit', handleForm);
+
+
+function paragraphClue(clue) {
+    cluesElement.innerHTML = clue;
+}
+
+function updateClick() {
     let inputValue = parseInt(inputElement.value);
 
     if (inputValue < 1 || inputValue > 100) {
-        cluesElement.innerHTML = "El número deber ser entre el 1 y 100, vuelve a intentar";
+        paragraphClue("El número deber ser entre el 1 y 100, vuelve a intentar");
     } else if (inputValue === random) {
-        cluesElement.innerHTML ="¡Has ganado campeona!";
+        paragraphClue("¡Has ganado campeona!");
+        btnElement.classList.toggle('hidden');
     } else if (inputValue > random) {
-        cluesElement.innerHTML = "Pista : Demasiado alto... prueba otra vez!"
+        paragraphClue("Pista : Demasiado alto... prueba otra vez!");
     } else {
-        cluesElement.innerHTML = "Pista: Demasiado bajo...prueba con otro número!"
+        paragraphClue("Pista: Demasiado bajo...prueba con otro número!");
     }
 }
 
-//Listener para el botón
-btnElement.addEventListener ("click", updateClick);
+let counter = 0;
+function updateAttempts() {
+    if (paragraphClue === "¡Has ganado campeona!") {
+        counter += 0;
+        attemptsElement.innerHTML = 'Numero de intentos:' + counter;
+    } else {
+        counter += 1;
+        attemptsElement.innerHTML = 'Numero de intentos:' + counter;
+    }
+}
 
+function handleButton() {
+    updateAttempts();
+    updateClick();
+};
+
+//Listener para el botón
+btnElement.addEventListener("click", handleButton);
